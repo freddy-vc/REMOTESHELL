e1cbc68beaa053e0811893a1cc6ebbb72ee70fc6
unknown <cabrerajuanicolas@gmail.com>
2025-05-31 14:54:12 -0500

    cambios 2.5

--- server/archivos.go
+++ server/archivos.go
@@ -8,42 +8,38 @@ import (
 
 var nombreArchi string = "users.db"
 
-func main() {
-	//crearArchi()
-	leerArchi()
-	modificarArchi()
-}
-
-func crearArchi() {
+func CrearArchivoUsuarios() {
 	mensaje := []byte("felipe:caf90169eefa5f807d577486b9f795ab86ae2983c5c20806cff959117e90af18\nmaria:caf90169eefa5f807d577486b9f795ab86ae2983c5c20806cff959117e90af18\nluis:caf90169eefa5f807d577486b9f795ab86ae2983c5c20806cff959117e90af18\n")
-
 	os.WriteFile(nombreArchi, mensaje, 0777)
 }
 
-func leerArchi() {
-
+func LeerArchivoUsuarios() {
 	dbUsers, _ := os.ReadFile(nombreArchi)
 	sDBusers := string(dbUsers)
-	//fmt.Println(sDBusers)
 
 	sDBusers = strings.TrimRight(sDBusers, "\n")
 	cuentas := strings.Split(sDBusers, "\n")
 
 	for _, credenciales := range cuentas {
-		//separar parametros de la cuenta
 		parametros := strings.Split(credenciales, ":")
 		fmt.Println("User = ", parametros[0])
 		fmt.Println("pass = ", parametros[1])
 	}
 }
 
-func modificarArchi() {
-	fmt.Println("...Agregando usuario...")
-	newUser := "jose:caf90169eefa5f807d577486b9f795ab86ae2983c5c20806cff959117e90af18\n"
-	dbUsers, _ := os.OpenFile(nombreArchi, os.O_APPEND|os.O_WRONLY, 0777)
-	dbUsers.WriteString(newUser)
-	dbUsers.Close()
-	fmt.Println("...Nuevo contenido...")
+func AgregarUsuario(username, passwordHash string) error {
+	newUser := fmt.Sprintf("%s:%s\n", username, passwordHash)
+	dbUsers, err := os.OpenFile(nombreArchi, os.O_APPEND|os.O_WRONLY, 0777)
+	if err != nil {
+		return fmt.Errorf("error al abrir archivo de usuarios: %v", err)
+	}
+	defer dbUsers.Close()
 
-	leerArchi()
+	_, err = dbUsers.WriteString(newUser)
+	if err != nil {
+		return fmt.Errorf("error al escribir nuevo usuario: %v", err)
+	}
+
+	fmt.Printf("Usuario %s agregado exitosamente\n", username)
+	return nil
 }
